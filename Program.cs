3cb835757f109704d5343ea7656857f9b681efbc
kkrystek-git <66742684+kkrystek-git@users.noreply.github.com>
2020-07-22 14:25:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Krystek_Lab_2_4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			int myFirstNumber = 16;
+			int mySecondNumber = 244;
+			Console.WriteLine(myFirstNumber * mySecondNumber);
+
+			string myText = "Some Text";
+			Console.WriteLine(myText + " and " + mySecondNumber);
+
+			string moreText = "Goes Here";
+			Console.WriteLine(myText + moreText);
+
+			string color1 = "Yellow";
+			string color2 = "Green";
+			bool isYellow = true;
+			bool isGreen = false;
+			Console.WriteLine(color1 + " " + isYellow + " " + color2 + " " + isGreen);
+
+			double numbers;
+			numbers = mySecondNumber/myFirstNumber;
+			Console.WriteLine(numbers);
+
+			double myFirstNumber2 = 16;
+			numbers = mySecondNumber / myFirstNumber2;
+			Console.WriteLine(numbers);
+
+			Console.WriteLine("BreakPoint Here");
+
+			string textToNumber = "80473";
+			int results = 99 * int.Parse(textToNumber);
+			Console.WriteLine(results);
+		}
+	}
+}
